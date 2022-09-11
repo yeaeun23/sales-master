1cb3bda8621ad6a5bfe07ea46ac9f043f2baf013
yeaeun23 <yeaeun23@naver.com>
2022-09-11 21:08:57 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '강예은',
+    'birthday': '940323',
+    'gender': '여자',
+    'job': '직장인'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '김민지',
+    'birthday': '940909',
+    'gender': '여자',
+    'job': '직장인'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '권소리',
+    'birthday': '940710',
+    'gender': '여자',
+    'job': '직장인'
+  },]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender} />
+          );
+        })
+      }
     </div>
   );
 }
