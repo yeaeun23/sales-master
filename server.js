ee75fcedd15c18606a8ced55fe777555c8a3bcfd
yeaeun23 <yeaeun23@naver.com>
2022-09-23 22:01:52 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
@@ -6,32 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          'id': 1,
-          'image': 'https://placeimg.com/64/64/1',
-          'name': '강예은',
-          'birthday': '940323',
-          'gender': '여자',
-          'job': '직장인'
-        },
-        {
-          'id': 2,
-          'image': 'https://placeimg.com/64/64/2',
-          'name': '김민지',
-          'birthday': '940909',
-          'gender': '여자',
-          'job': '직장인'
-        },
-        {
-          'id': 3,
-          'image': 'https://placeimg.com/64/64/3',
-          'name': '권소리',
-          'birthday': '940710',
-          'gender': '여자',
-          'job': '직장인'
-        }])
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
