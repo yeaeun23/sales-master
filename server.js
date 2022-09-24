7ba0e6efffe55440bca3cd66fa5c7021022a986a
yeaeun23 <yeaeun23@naver.com>
2022-09-24 23:30:55 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
+const { urlToHttpOptions } = require('url');
 
 const connection = mysql.createConnection({
   host: conf.host,
@@ -21,12 +22,31 @@ const connection = mysql.createConnection({
 connection.connect();
 
 app.get('/api/customers', (req, res) => {
-    connection.query(
-      "SELECT * FROM CUSTOMER",
-      (err, rows, fields) => {
-        res.send(rows);
-      }
-    )
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  )
+});
+
+app.post('/api/customers', (req, res) => {
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  console.log(name);
+  console.log(birthday);
+  console.log(gender);
+  console.log(job);
+  let params = ['', name, birthday, gender, job];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+      console.log(err);
+      console.log(err);
+    })
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
